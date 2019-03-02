e93e05bd483be64d99a6cd632b3bce45a739defa
dimorinny <didika914@gmail.com>
2019-03-03 00:22:53 +0300

    dialing connection with golang context

--- grpc.go
+++ grpc.go
@@ -14,33 +14,20 @@ func grpcContext() (context.Context, context.CancelFunc) {
 	)
 }
 
-// TODO: use dial context instead deprecated dial
 func dial(
 	unixSocketPath string,
 	ctx context.Context,
 ) (*grpc.ClientConn, error) {
-	//connection, err := grpc.DialContext(
-	//	ctx,
-	//	unixSocketPath,
-	//	grpc.WithBlock(),
-	//	grpc.WithInsecure(),
-	//)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//
-	//return connection, nil
-
-	connection, err := grpc.Dial(
+	connection, err := grpc.DialContext(
+		ctx,
 		unixSocketPath,
-		grpc.WithInsecure(),
 		grpc.WithBlock(),
-		grpc.WithTimeout(time.Second*5),
-		grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
-			return net.DialTimeout("unix", addr, timeout)
+		grpc.WithInsecure(),
+		grpc.WithContextDialer(func(ctx context.Context, addr string) (net.Conn, error) {
+			var d net.Dialer
+			return d.DialContext(ctx, "unix", addr)
 		}),
 	)
-
 	if err != nil {
 		return nil, err
 	}
