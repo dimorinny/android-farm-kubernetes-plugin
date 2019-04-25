854c44188753ef8cb202105b7fdaf491c04aa904
damerkurev <dmerkurev@avito.ru>
2019-04-25 04:27:43 +0300

    forward adbd using adb forward and socat

--- example/device/entrypoint.sh
+++ example/device/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+set -eux
+
+# Forward adbd port from device to host
+adb forward tcp:5555 tcp:5555
+
 # Detect ip and forward ADB ports outside to outside interface
 ip=$(ifconfig eth0 | grep 'inet' | cut -d: -f2 | awk '{ print $1}')
-socat tcp-listen:5037,bind=$ip,fork tcp:127.0.0.1:5037 &
-socat tcp-listen:5554,bind=$ip,fork tcp:127.0.0.1:5554 &
-socat tcp-listen:5555,bind=$ip,fork tcp:127.0.0.1:5555 &
+socat tcp-listen:5555,bind=${ip},fork tcp:127.0.0.1:5555 &
 
 # Sleep forever
 tail -f /dev/null
